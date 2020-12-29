a102cc3d9307c56460327fffa64ea5e1baff3bc3
nick_richardson <nick.richardson@rezonence.com>
2020-12-29 19:35:07 +0000

    Unit test neighbour finding

--- src/matrix updater/matrix.updater.spec.ts
+++ src/matrix updater/matrix.updater.spec.ts
@@ -1,6 +1,7 @@
 import {MatrixUpdater} from "./matrix.updater";
 import {CellStateCalculator} from "../cell-state calculator/cell.state.calculator";
 import expect from "expect";
+import {FindNeighboursRequest} from "./find.neighbours.request";
 
 describe("Matrix updater", () => {
     let updater: MatrixUpdater;
@@ -29,5 +30,26 @@ describe("Matrix updater", () => {
         matrix.forEach((row, index) => {
             expect(row.length).toBe(matrix[index].length);
         })
+    })
+
+    it("Finds neighbours", () => {
+        let cellIndex = 0;
+        let neighbours: boolean[];
+        const request: FindNeighboursRequest = {
+            previousRow: [true, false, false],
+            currentRow: [false, true, false],
+            nextRow: [false, false, false],
+            cellIndex
+        }
+        neighbours = updater.toNeighbours(request);
+        expect(neighbours.length).toBe(5);
+        expect(neighbours.filter(n => n === true).length).toBe(2);
+        expect(neighbours.filter(n => n === false).length).toBe(3);
+
+        cellIndex = 1;
+        neighbours = updater.toNeighbours({...request, cellIndex});
+        expect(neighbours.length).toBe(8);
+        expect(neighbours.filter(n => n === true).length).toBe(1);
+        expect(neighbours.filter(n => n === false).length).toBe(7)
     })
 })
--- src/matrix updater/matrix.updater.ts
+++ src/matrix updater/matrix.updater.ts
@@ -9,16 +9,13 @@ export class MatrixUpdater {
     updateMatrix(matrix: (boolean[])[]): (boolean[])[] {
         return matrix.map((row, rowIndex) => {
             return row.map((cellState, cellIndex) => {
-                console.log("ROW", row);
                 const request = {
                     previousRow: matrix[rowIndex - 1],
                     currentRow: row,
                     nextRow: matrix[rowIndex + 1],
                     cellIndex
                 };
-                console.log("CELL INDEX", cellIndex);
                 const neighbours = this.toNeighbours(request);
-                console.log("NEIGHBS", neighbours);
                 return this.cellStateCalculator.calculateCellState(cellState, neighbours);
             })
         })
